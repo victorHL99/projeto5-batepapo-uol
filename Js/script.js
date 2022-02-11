4a91eae2da24ac81c8685e7c78034b97d5a40ec1
Victor Hugo Lessa <victor.lessa@estudante.ufla.br>
2022-02-11 14:30:49 -0300

    Fixed message request and layout bug

--- Js/script.js
+++ Js/script.js
@@ -1,17 +1,60 @@
+let name;
+
 /*REQUEST */
-const messagePromise = axios.get('https://mock-api.driven.com.br/api/v4/uol/messages')
-messagePromise.then(showMessage)
+selectName();
+function selectName() {
+    nome = prompt("Qual o seu nome?");
+    const messagePromise = axios.get('https://mock-api.driven.com.br/api/v4/uol/messages')
+    messagePromise.then(showMessage)
+}
 
-console.log(messagePromise)
+function displayStatus(message) {
+    const public = (message.type !== "private_message" || message.to === "Todos")
+    const private = (message.to === nome || message.from === nome)
 
-function showMessage(response){
-    let enterExit = document.querySelector(".enter-exit");
-    let normal = document.querySelector(".normal");
-    let private = document.querySelector(".private")
+    return (public||private)
+}
 
-    enterExit.innerHTML = response.data.status;
-    normal.innerHTML = response.data.message;
-    private.innerHTML = response.data.private_menssage
+/*function to request message */
+function showMessage(responseMessage) {
+    let i = 0
+    let messagens = responseMessage.data.filter(displayStatus);
+    let containerMain = document.querySelector(".container");
+    containerMain.innerHTML = "";
 
-    console.log(response.data)
+    for (i = 0;i <= messagens.length; i++ ){
+        const messagerr = messagens[i];
+        if(messagerr.type === 'status' ){
+            containerMain.innerHTML +=`<div class="enter-exit">
+            <div class="horario"> (${messagerr.time})   </div>
+            <div class="nome">&nbsp${messagerr.from}</div>:
+            <div class="texto">&nbsp${messagerr.text}</div>
+            </div>
+            `
+
+        } else if(messagerr.type === 'private_message'){
+            containerMain.innerHTML += `
+            <div class="private">
+                <div class="horario"> (${messagerr.time})  </div>
+                <div class="nome">&nbsp${nome}&nbsp</div>
+                <span>reservadamente para </span>
+                <div class="nome">&nbsp${messagerr.from}  </div>:
+                <div class="texto">&nbsp${messagerr.text}  </div>
+            </div>
+            `
+        } else if(messagerr.type === 'message'){
+            containerMain.innerHTML += `
+            <div class="public">
+                <div class="horario"> (${messagerr.time})  </div>
+                <div class="nome">&nbsp ${messagerr.from}&nbsp</div> 
+                <span> para </span>
+                <div class="nome"> &nbsp${messagerr.to}  </div>:
+                <div class="texto"> &nbsp${messagerr.text}  </div>
+            </div>
+            `
+        }
+    }
+
+
+    
 }
